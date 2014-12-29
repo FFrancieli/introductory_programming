df041a35dd637654186ed7749ce378cda058cee4
FFrancieli <francielifrv@gmail.com>
2014-12-29 13:42:26 -0200

    Exercise 2
    Vertical Line

---
+++ src/Exercises/Ex2_VerticalLine.java
@@ -0,0 +1,16 @@
+package Exercises;
+
+/**
+ * Created by Francieli on 29/12/2014.
+*/
+public class Ex2_VerticalLine {
+    public static void main(String[] args) {
+        printVerticalLine(8, "*");
+    }
+
+    private static void printVerticalLine(int numberOfSymbols, String symbol){
+        for (int i = 0; i < numberOfSymbols; i++){
+            System.out.println(symbol);
+        }
+    }
+}
