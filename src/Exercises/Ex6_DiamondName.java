b155f98664761d06b229157f4b8f8e478aead52d
FFrancieli <francielifrv@gmail.com>
2014-12-30 01:13:27 -0200

    Exercise 6
    Diamond with name

---
+++ src/Exercises/Ex6_DiamondName.java
@@ -0,0 +1,70 @@
+package Exercises;
+
+/**
+ * Created by Francieli on 30/12/2014.
+ */
+public class Ex6_DiamondName {
+    public static void main(String[] args) {
+        int n = 4;
+        drawTriangleTop(n);
+        drawTriangleDown(n - 1);
+
+    }
+
+
+    private static void drawTriangleTop(int n){
+
+        int numberOfSpaces = n - 1;
+        int numberOfSymbols = 1;
+        String str = "";
+
+        for (int i = 1; i <= n; i++) {
+
+            str += insertWhiteSpace(numberOfSpaces);
+
+            for (int j = 0; j < numberOfSymbols; j++) {
+                if (numberOfSymbols == 2*n-1){
+                    str = "Francieli ";
+                }else {
+                    str += "*";
+                }
+            }
+
+            numberOfSymbols += 2;
+            numberOfSpaces--;
+
+            System.out.println(str);
+            str = "";
+        }
+
+    }
+
+    private static void drawTriangleDown(int n){
+        int numberOfSpaces = 1;
+        int numberOfSymbols = (n - 1) * 2  + 1;
+        String str = "";
+
+        for (int i = 1; i <= n; i++){
+
+            str += insertWhiteSpace(numberOfSpaces);
+
+            for (int j = 0; j < numberOfSymbols; j++){
+                str += "*";
+            }
+
+            numberOfSymbols -= 2;
+            numberOfSpaces ++;
+
+            System.out.println(str);
+            str = "";
+        }
+    }
+
+    private static String insertWhiteSpace(int numberOfWhiteSpaces) {
+        String str = "";
+        for (int i = 0; i < numberOfWhiteSpaces; i++) {
+            str = " " + str;
+        }
+        return str;
+    }
+}
